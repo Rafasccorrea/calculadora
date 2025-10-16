391f222f4c9f20c4c94801e9a0ca8a3dd93f1b99
Rafasccorrea <rafaela.silva.correa@escola.pr.gov.br>
2025-10-16 11:22:20 -0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,21 +1,23 @@
 function adicionar(valor) {
     document.getElementById('visor').value += valor;
-    
+    }
+
     function limpar() {
-    document.getElementById('visor').value = '';
-    
-    function apagar() {
+        document.getElementById('visor').value = '';
+    }
+
+function apagar() {
     var visor = document.getElementById('visor');
     visor.value = visor.value.slice(0, -1);
-    
-    function calcular() {
+    }
+
+function calcular() {
     var visor = document.getElementById('visor');
     try {
-    
-    
+
     visor.value = eval(visor.value);
-    } catch (error) {
-    
+} catch (error) {
+        
     visor.value = 'Erro';
     }
-    }
+}
